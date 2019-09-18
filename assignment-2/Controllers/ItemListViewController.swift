02227bfa59d8d150148f6f210fe76d1c5539bf17
Anushka Yohan <aliyanage44@gmail.com>
2019-09-18 14:10:19 +0530

    Design table view cell as a separate nib

---
+++ assignment-2/Controllers/ItemListViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ItemListViewController.swift
+//  assignment-2
+//
+//  Created by Anushka Liyanage on 9/18/19.
+//  Copyright © 2019 Anushka Liyanage. All rights reserved.
+//
+
+import Foundation
