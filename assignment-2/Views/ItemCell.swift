15126a7ff709232f25e7d4417371b12d9be63286
Anushka Yohan <aliyanage44@gmail.com>
2019-09-18 14:10:47 +0530

    Make an owner class for custom table cell nib

--- assignment-2/Views/ItemCell.swift
+++ assignment-2/Views/ItemCell.swift
@@ -10,27 +10,7 @@ import UIKit
 
 class ItemCell: UITableViewCell {
 
+    @IBOutlet weak var thumbnail: UIImageView!
     @IBOutlet weak var itemName: UILabel!
     @IBOutlet weak var itemPrice: UILabel!
-    @IBOutlet weak var thumbnail: UIImageView!
-    
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
